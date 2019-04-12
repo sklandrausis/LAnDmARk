d37a39f9328bfc8eb4c67defe3d0eb2983b6243c
Janis <janis.akmenskanlns@gmail.com>
2019-04-12 10:34:59 +0200

    save

--- startStaging.py
+++ startStaging.py
@@ -1,53 +1,42 @@
+import os
+from awlofar.toolbox.LtaStager import LtaStager, LtaStagerError
 from awlofar.main.aweimports import *
-from awlofar.toolbox.LtaStager import LtaStager, LtaStagerError
-import awlofar.main as mmm
-from datetime import datetime
-from awlofar.database.Context import context
-from awlofar.main.aweimports import CorrelatedDataProduct, FileObject, Observation
-from awlofar.toolbox.LtaStager import LtaStager, LtaStagerError
 
-do_stage = True
-project = 'LC4_010'
-#RA = 277.3822
-#DEC = 48.7464
+from parsers._configparser import ConfigParser
 
-#SASid = 366346
+def getConfigs(key, value):
+    configFilePath = "config.cfg"
+    config = ConfigParser.getInstance()
+    config.CreateConfig(configFilePath)
+    return config.getConfig(key, value)
 
-uris = {
-    project: set()
-}
+if __name__ == "__main__":
+    do_stage = False
+    uris = set()
+    SASid = int(getConfigs("Project", "SASid"))
+    print("SAS id", SASid)
+    targetName = getConfigs("Project", "TargetName")
+    print("Target name", targetName)
+    cls = CorrelatedDataProduct
+    queryObservations = (getattr(Process, "observationId") == SASid) & (Process.isValid > 0)
 
-cls = CorrelatedDataProduct
-query_observations = Observation.select_all().project_only(project)
+    for observation in queryObservations:
+        print("Querying ObservationID",  observation.observationId)
+        dataproduct_query = cls.observations.contains(observation)
+        dataproduct_query &= cls.subArrayPointing.targetName == targetName
 
-for observation in query_observations:
-    print("Querying ObservationID %s" % observation.observationId)
+        for dataproduct in dataproduct_query:
+            fileobject = ((FileObject.data_object == dataproduct) & (FileObject.isValid > 0)).max('creation_date')
+            if fileobject:
+                uris.add(fileobject.URI)
+                print("URI found", fileobject.URI)
+            else:
+                print("No URI found for %s with dataProductIdentifier", (dataproduct.__class__.__name__, dataproduct.dataProductIdentifier))
 
-    dataproduct_query = cls.observations.contains(observation)
-    dataproduct_query &= cls.isValid == 1
+    print("Total URI's found %d" % len(uris))
 
-    '''
-    for process in query:
-        Observation.Process = process
-        query_subarr = SubArrayPointing.pointing == pointing
-        for subarr in query_subarr:
-            query_obs = Observation.subArrayPointings.contains(subarr)
-        print(process)
-    '''
+    os.system("rm " + "*.log")
 
-    for dataproduct in dataproduct_query:
-        fileobject = ((FileObject.data_object == dataproduct) & (FileObject.isValid > 0)).max('creation_date')
-
-        if fileobject:
-            #print("URI found %s" % fiurisleobject.URI)
-            uris[project].add(fileobject.URI)
-        else:
-            print("No URI found for %s with dataProductIdentifier %d" % (dataproduct.__class__.__name__, dataproduct.dataProductIdentifier))
-
-    print("Total URI's found for project %s: %d" % (project, len(uris[project])))
-
-stager = LtaStager()
-if do_stage:
-    print("stage")
-    stager.stage_uris(uris[project])
-
+    if do_stage:
+        stager = LtaStager()
+        stager.stage_uris(uris)
