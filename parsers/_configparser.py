a0044916b2fc20ffce1c94a887c19f3da11a1a99
Janis <janis.akmenskanlns@gmail.com>
2019-04-30 11:52:59 +0300

    change config parser

--- parsers/_configparser.py
+++ parsers/_configparser.py
@@ -4,33 +4,7 @@ try:
 except:
     import Configparser
 
-class ConfigParser():
-    __instance = None
-    
-    @staticmethod 
-    def getInstance():
-        if ConfigParser.__instance == None:
-            ConfigParser()
-        return ConfigParser.__instance
-    
-    def __init__(self):
-        if ConfigParser.__instance != None:
-            raise Exception("This class is a singleton!")
-        else:
-            ConfigParser.__instance = self
-            
-    def CreateConfig(self, configFilePath):
-        self.configFilePath = configFilePath
-        __config = configparser.RawConfigParser()
-        __config.read(configFilePath)
-        self.config = __config
-          
-    def getConfig(self, key, value):
-        _config = self.config.get(key, value)
-        return _config
-
 def getConfigs(key, value, configFilePath):
-    configFilePath = configFilePath
-    config = ConfigParser.getInstance()
-    config.CreateConfig(configFilePath)
-    return config.getConfig(key, value)
+    config = configparser.RawConfigParser()
+    config.read(configFilePath)
+    return config.get(key, value)
