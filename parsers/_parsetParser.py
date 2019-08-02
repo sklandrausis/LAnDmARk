ebe31a96d8aefb8d765fa88c4c7754e560c18c0f
Sklandrausis <janis.akmenskalns@gmail.com>
2019-08-02 10:04:18 +0300

    Fix setup.py

--- parsers/_parsetParser.py
+++ parsers/_parsetParser.py
@@ -44,7 +44,7 @@ class ParsetParser():
                 if str(l) in self.comentsDict.keys() and  str(l) in self.params.keys():
                     s1 = self.params[str(l)].ljust(27, " ") + " = " + self.parsetDict[self.params[str(l)]]
                     s2 = "#" + self.comentsDict[str(l)][0]
-                    s = s1.ljust(90, " ") + s2
+                    s = s1.ljust(100, " ") + s2
                     #s.replace("{{", "{{ ")
                     #s.replace("}}", " }}")
                     s.replace("#  #", "##")
