93965e9d26f57f2c0d0819a9c07ba08d0e691158
Sklandrausis <janis.akmenskalns@gmail.com>
2019-08-12 12:49:49 +0300

    add total runtime to main.py

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import time
 from parsers._configparser import getConfigs
 
 if __name__ == "__main__":
+    start_time_main = time.time()
+
     os.system("python3 " + "startStaging.py")
     if getConfigs("Operations", "Retrieve", "config.cfg") == "True":
         start_data_retrive_time = time.time()
@@ -17,3 +19,6 @@ if __name__ == "__main__":
         os.system("python3 " + "runPipelins.py")
         end_data_process_time = time.time()
         print("Data selection time", end_data_process_time - start_data_process_time)
+
+    end_time_main = time.time()
+    print("Total time ", end_time_main - start_time_main)
