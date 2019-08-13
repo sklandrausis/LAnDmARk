786d42b6add685529849a8627d41cc01725e6370
Sklandrausis <janis.akmenskalns@gmail.com>
2019-08-13 16:30:54 +0300

    improve

--- main2.py
+++ main2.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     os.system("python3 " + "startStaging2.py")
     if getConfigs("Operations", "Retrieve", "config.cfg") == "True":
         start_data_retrive_time = time.time()
-        os.system("python3 " + "downloadDataproducts2.py")
+        os.system("python3 " + "downloadDataproducts.py2")
         end_data_retrive_time = time.time()
         print("Data selection time", end_data_retrive_time  - start_data_retrive_time)
 
@@ -20,6 +20,5 @@ if __name__ == "__main__":
         end_data_process_time = time.time()
         print("Data download time", end_data_process_time - start_data_process_time)
 
-    end_time_main =time.time()
+    end_time_main = time.time()
     print("Total time ", end_time_main - start_time_main)
-
