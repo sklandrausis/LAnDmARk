0cbc2038678ff0c9933a2c6de010e316b60aaeba
Kristapsv <kriksis300@gmail.com>
2019-09-17 16:29:07 +0200

    Update downloadDataproducts.py

--- downloadDataproducts.py
+++ downloadDataproducts.py
@@ -2,14 +2,16 @@ import time
 
 from parsers._configparser import getConfigs
 from stager_access import *
+
+from pylive import live_plotter
 from progress import progress
 import numpy as np
 import matplotlib.pyplot as plt
+from parsers._configparser import getConfigs
 import argparse
 
-
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='''Executes all scripts. ''', epilog="""Main""")
+    parser = argparse.ArgumentParser(description='''download data products. ''', epilog="""Download""")
     parser.add_argument("-c", "--config", help="Configuration cfg file", type=str, default="config.cfg")
     parser.add_argument("-v", "--version", action="version", version='%(prog)s - Version 1.0')
     args = parser.parse_args()
@@ -19,11 +21,14 @@ def parse_arguments():
 def get_args(key):
     return str(parse_arguments().__dict__[key])
 
-
 if __name__ == "__main__":
-    config_file = get_args("config")
     tmpStagesIDs = set([])
-    i = 0.0
+    config_file = get_args("config")
+    
+    #progress is 0
+    i=0.0
+	
+    #plot initial values
     percent_done = []
     files_done = []
 
@@ -36,6 +41,7 @@ if __name__ == "__main__":
 
             for stageID in stagesIDs:
                 status = progess[stageID]["Status"]
+				
                 progress(i, 100, status='Staging in progress')
                 i=float(progess[stageID]["Percent done"])
                 f=float(progess[stageID]["Files done"])
@@ -46,7 +52,7 @@ if __name__ == "__main__":
         else:
             for id in tmpStagesIDs:
                 surl = get_surls_online(int(id))
-                SASidsTarget = [int(id) for id in getConfigs("Data", "targetSASids", config_file).replace(" ", "").split(",")]
+                SASidsTarget = [int(id) for id in getConfigs("Data", "targetSASids", "config.cfg").replace(" ", "").split(",")]
 
                 project = getConfigs("Data", "PROJECTid", "config.cfg")
                 if len(getConfigs("Data", "calibratorSASids", "config.cfg")) == 0:
@@ -57,15 +63,19 @@ if __name__ == "__main__":
                         raise Exception("SAS id for calibrator is not set in config.cfg file")
                         sys.exit(1)
                 else:
-                    SASidsCalibrator = [int(id) for id in getConfigs("Data", "calibratorSASids", config_file).replace(" ", "").split(",")]
+                    SASidsCalibrator = [int(id) for id in getConfigs("Data", "calibratorSASids", "config.cfg").replace(" ", "").split(",")]
 
-                download(surl, getConfigs("Paths", "WorkingPath", "config.cfg") + "/" + getConfigs("Data", "TargetName", config_file) + "/", SASidsCalibrator, SASidsTarget, )
+                download(surl, getConfigs("Paths", "WorkingPath", "config.cfg") + "/" + getConfigs("Data", "TargetName", "config.cfg") + "/", SASidsCalibrator, SASidsTarget, )
 
             break
 
         time.sleep(1)
 
     if percent_done:
+        workingDir = getConfigs("Paths", "WorkingPath", config_file)
+        targetName = getConfigs("Data", "TargetName", config_file)
+        workingDir = workingDir + "/" + targetName + "/"
+        auxDir = workingDir + "/LAnDmARk_aux"
         #create and save the plot.
         plt.style.use('ggplot')
         plt.ylabel('Percent')
@@ -74,22 +84,13 @@ if __name__ == "__main__":
         y = np.array(percent_done)
         x = np.array(range(0, len(percent_done)))
         plt.plot(x,y)
-		
-        plt.savefig('percentDone.png')
-        plt.clf()
-		
-        
-    if files_done:
-        #create and save the plot.
-        plt.style.use('ggplot')
+        plt.twinx()
+        y1 = np.array(files_done)
         plt.ylabel('Files')
-        plt.xlabel('Seconds')
-        plt.title('Files done')
-        y = np.array(files_done)
-        x = np.array(range(0, len(files_done)))
-        plt.plot(x,y)
-		
-        plt.savefig('filesDone.png')
+        plt.plot(x,y1)
+        
+        plt.savefig(auxDir + "/staging/" + 'percentDone.png')
         plt.clf()
+		
 	
     print("done")
