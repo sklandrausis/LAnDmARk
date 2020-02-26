1c0d2e5074da95900361dd1e7b19ad6a023fe641
sklandrausis <janis.akmenskalns@gmail.com>
2020-02-26 15:10:16 +0200

    add contrains to mainGUI

--- mainGUI.py
+++ mainGUI.py
@@ -2,10 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from sys import version_info
+import os
+import time
 from PyQt5.QtWidgets import (QWidget, QGridLayout, QApplication, QDesktopWidget, QPushButton, QLabel, QComboBox, QLineEdit, QMessageBox)
 from PyQt5.QtGui import QFont
-
-from parsers._configparser import setConfigs
+from PyQt5 import QtCore
+import pyqtgraph as pg
+from stager_access import *
+from parsers._configparser import setConfigs, getConfigs
 
 
 class Landmark_GUI(QWidget):
@@ -25,21 +30,135 @@ class Landmark_GUI(QWidget):
         self.move(qr.topLeft())
 
     def create_init_view(self):
-        setup_button = QPushButton("Setup", self)
-        run_button = QPushButton("Run", self)
-        check_button = QPushButton("Check", self)
-        setup_button.setStyleSheet("background-color: yellow")
-        setup_button.clicked.connect(self.setup)
-        run_button.setStyleSheet("background-color: green")
-        check_button.setStyleSheet("background-color: blue")
-        self.grid.addWidget(setup_button, 0,0)
-        self.grid.addWidget(run_button, 1,0)
-        self.grid.addWidget(check_button, 2,0)
+        self.setup_button = QPushButton("Setup", self)
+        self.run_button = QPushButton("Run", self)
+        self.check_button = QPushButton("Check", self)
+        self.setup_button.setStyleSheet("background-color: yellow")
+        self.setup_button.clicked.connect(self.setup)
+        self.run_button.setStyleSheet("background-color: green")
+        self.run_button.clicked.connect(self.run_landmark)
+        self.check_button.setStyleSheet("background-color: blue")
+        self.grid.addWidget(self.setup_button, 0,0)
+        self.grid.addWidget(self.run_button, 1,0)
+        self.grid.addWidget(self.check_button, 2,0)
 
         self.init_label = QLabel("LAnDmARk GUI")
         init_label_font = QFont("Times New Roman", 30, QFont.Bold)
         self.init_label.setFont(init_label_font)
         self.grid.addWidget(self.init_label, 1,1)
+
+    def validate_setup(self, targetSASid, targetname, projectID, max_per_node, method, WorkingPath, PrefactorPath, lofarroot, casaroot, pyraproot, losotoPath, aoflagger, wsclean_executable, pythonpath, task_file):
+        valid_count = 0
+        if len(targetSASid) == 0:
+            valid_count += 1
+            QMessageBox.warning(self, "Warning", "Target SAS id cannot be empty")
+
+        if len(targetname) == 0:
+            valid_count += 1
+            QMessageBox.warning(self, "Warning", "Target name cannot be empty")
+
+        if len(projectID) == 0:
+            valid_count += 1
+            QMessageBox.warning(self, "Warning", "Project ID cannot be empty")
+
+        if len(max_per_node) == 0:
+            valid_count += 1
+            QMessageBox.warning(self, "Warning", "max_per_node cannot be empty")
+
+        if len(method) == 0:
+            valid_count += 1
+            QMessageBox.warning(self, "Warning", "method cannot be empty")
+
+        if len(WorkingPath) == 0:
+            valid_count += 1
+            QMessageBox.warning(self, "Warning", "Working Pathcannot be empty")
+
+        if len(PrefactorPath) == 0:
+            valid_count += 1
+            QMessageBox.warning(self, "Warning", "PrefactorPath cannot be empty")
+
+        if len(lofarroot) == 0:
+            valid_count += 1
+            QMessageBox.warning(self, "Warning", "lofarroot cannot be empty")
+
+        if len(casaroot) == 0:
+            valid_count += 1
+            QMessageBox.warning(self, "Warning", "casaroot cannot be empty")
+
+        if len(pyraproot) == 0:
+            valid_count += 1
+            QMessageBox.warning(self, "Warning", "pyraproot cannot be empty")
+
+        if len(losotoPath) == 0:
+            valid_count += 1
+            QMessageBox.warning(self, "Warning", "losotoPath cannot be empty")
+
+        if len(aoflagger) == 0:
+            valid_count += 1
+            QMessageBox.warning(self, "Warning", "aoflagger cannot be empty")
+
+        if len(wsclean_executable) == 0:
+            valid_count += 1
+            QMessageBox.warning(self, "Warning", "wsclean_executable cannot be empty")
+
+        if len(pythonpath) == 0:
+            valid_count += 1
+            QMessageBox.warning(self, "Warning", "pythonpath cannot be empty")
+
+        if len(task_file) == 0:
+            valid_count += 1
+            QMessageBox.warning(self, "Warning", "task_file cannot be empty")
+
+        if valid_count == 0:
+            valid = True
+        else:
+            valid = False
+        return valid
+
+    def run_landmark(self):
+        self.setWindowTitle("Running ...")
+        for w in self.setup_wigets:
+            if type(w) is QLabel or type(w) is QLineEdit or type(w) is QComboBox:
+                w.clear()
+
+            w.close()
+            w.destroy()
+            self.grid.removeWidget(w)
+
+        import collections
+        import numpy as np
+        sampleinterval = 0.1
+        timewindow = 10
+        self._interval = int(sampleinterval * 1000)
+        self._bufsize = int(timewindow / sampleinterval)
+        self.databuffer = collections.deque([0.0] * self._bufsize, self._bufsize)
+        self.x = np.linspace(-timewindow, 0.0, self._bufsize)
+        self.y = np.zeros(self._bufsize, dtype=np.float)
+        self.plt = pg.PlotWidget()
+        self.plt.plot(title='Dynamic Plotting with PyQtGraph')
+        #self.plt.resize(*size)
+        self.plt.showGrid(x=True, y=True)
+        self.plt.setLabel('left', 'amplitude', 'V')
+        self.plt.setLabel('bottom', 'time', 's')
+        self.curve = self.plt.plot(self.x, self.y, pen=(255, 0, 0))
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.updateplot)
+        self.timer.start(self._interval)
+        self.grid.addWidget(self.plt)
+
+
+    def getdata(self):
+        import math, random
+        frequency = 0.5
+        noise = random.normalvariate(0., 1.)
+        new = 10. * math.sin(time.time() * frequency * 2 * math.pi) + noise
+        return new
+
+    def updateplot(self):
+            self.databuffer.append(self.getdata())
+            self.y[:] = self.databuffer
+            self.curve.setData(self.x, self.y)
+            #self.processEvents()
 
     def setup(self):
         self.setWindowTitle("Setup")
@@ -214,43 +333,46 @@ class Landmark_GUI(QWidget):
         self.grid.addWidget(self.task_file_label, 22, 12)
         self.grid.addWidget(self.task_file_input, 23, 12)
 
-        save_button = QPushButton("Save LAnDmARk configuration", self)
-        save_button.setStyleSheet("background-color: green")
-        save_button.clicked.connect(self.save_configuration)
-        self.grid.addWidget(save_button, 4, 0)
+        self.save_button = QPushButton("Save LAnDmARk configuration", self)
+        self.save_button.setStyleSheet("background-color: green")
+        self.save_button.clicked.connect(self.save_configuration)
+        self.grid.addWidget(self.save_button, 4, 0)
+
+        self.setup_wigets = set([self.opertion_label, self.querying_label, self.querying_combobox, self.stage_label, self.stage_combobox, self.retrive_label, self.retrive_combobox, self.process_label, self.process_combobox, self.which_obj_label, self.which_combobox, self.data_label, self.calibratorSASids_label, self.calibratorSASids_input, self.targetSASids_label, self.targetSASids_input, self.Target_name_label, self.Target_name_input, self.PROJECTid_label, self.PROJECTid_input, self.product_type_label, self.product_type_combobox, self.cluster_label, self.max_per_node, self.max_per_node_input, self.method_label, self.method_input, self.WorkingPath_label, self.WorkingPath_input, self.PrefactorPath_label, self.PrefactorPath_input, self.lofarroot_label, self.lofarroot_input, self.casaroot_label, self.casaroot_input, self.pyraproot_label, self.pyraproot_input, self.hdf5root_label, self.hdf5root_input, self.wcsroot_label, self.wcsroot_input, self.losotoPath_label, self.losotoPath_input, self.aoflagger_label, self.aoflagger_input, self.wsclean_executable_label, self.wsclean_executable_input, self.pythonpath_label, self.pythonpath_input, self.task_file_label, self.task_file_input, self.save_button])
 
     def save_configuration(self):
-        config_file = "config.cfg"
+        if self.validate_setup(self.targetSASids_input.text(), self.Target_name_input.text(), self.PROJECTid_input.text(), self.max_per_node_input.text(), self.method_input.text(), self.WorkingPath_input.text(), self.PrefactorPath_input.text(), self.lofarroot_input.text(), self.casaroot_input.text(), self.pyraproot_input.text(), self.losotoPath_input.text(), self.aoflagger_input.text(), self.wsclean_executable_input.text(), self.pythonpath_input.text(), self.task_file_input.text()   ):
+            config_file = "config.cfg"
 
-        setConfigs("Data", "targetSASids", self.targetSASids_input.text(), config_file)
-        setConfigs("Data", "calibratorsasids", self.calibratorSASids_input.text(), config_file)
-        setConfigs("Data", "targetname", self.Target_name_input.text(), config_file)
-        setConfigs("Data", "projectid", self.PROJECTid_input.text(), config_file)
-        setConfigs("Data", "producttype", self.product_type_combobox.currentText(), config_file)
+            setConfigs("Data", "targetSASids", self.targetSASids_input.text(), config_file)
+            setConfigs("Data", "calibratorsasids", self.calibratorSASids_input.text(), config_file)
+            setConfigs("Data", "targetname", self.Target_name_input.text(), config_file)
+            setConfigs("Data", "projectid", self.PROJECTid_input.text(), config_file)
+            setConfigs("Data", "producttype", self.product_type_combobox.currentText(), config_file)
 
-        setConfigs("Operations", "querying", self.querying_combobox.currentText(), config_file)
-        setConfigs("Operations", "stage", self.stage_combobox.currentText(), config_file)
-        setConfigs("Operations", "retrieve", self.retrive_combobox.currentText(), config_file)
-        setConfigs("Operations", "process", self.process_combobox.currentText(), config_file)
+            setConfigs("Operations", "querying", self.querying_combobox.currentText(), config_file)
+            setConfigs("Operations", "stage", self.stage_combobox.currentText(), config_file)
+            setConfigs("Operations", "retrieve", self.retrive_combobox.currentText(), config_file)
+            setConfigs("Operations", "process", self.process_combobox.currentText(), config_file)
 
-        setConfigs("Cluster", "max_per_node", self.max_per_node_input.text(), config_file)
-        setConfigs("Cluster", "method", self.method_input.text(), config_file)
+            setConfigs("Cluster", "max_per_node", self.max_per_node_input.text(), config_file)
+            setConfigs("Cluster", "method", self.method_input.text(), config_file)
 
-        setConfigs("Paths", "workingpath", self.WorkingPath_input.text(), config_file)
-        setConfigs("Paths", "prefactorpath", self.PrefactorPath_input.text(), config_file)
-        setConfigs("Paths", "lofarroot", self.lofarroot_input.text(), config_file)
-        setConfigs("Paths", "casaroot", self.casaroot_input.text(), config_file)
-        setConfigs("Paths", "pyraproot", self.pyraproot_input.text(), config_file)
-        setConfigs("Paths", "hdf5root", self.hdf5root_input.text(), config_file)
-        setConfigs("Paths", "wcsroot", self.wcsroot_input.text(), config_file)
-        setConfigs("Paths", "losotopath", self.losotoPath_input.text(), config_file)
-        setConfigs("Paths", "aoflagger", self.aoflagger_input.text(), config_file)
-        setConfigs("Paths", "wsclean_executable", self.wsclean_executable_input.text(), config_file)
-        setConfigs("Paths", "pythonpath", self.pythonpath_input.text(), config_file)
-        setConfigs("Paths", "task_file", self.task_file_input.text(), config_file)
+            setConfigs("Paths", "workingpath", self.WorkingPath_input.text(), config_file)
+            setConfigs("Paths", "prefactorpath", self.PrefactorPath_input.text(), config_file)
+            setConfigs("Paths", "lofarroot", self.lofarroot_input.text(), config_file)
+            setConfigs("Paths", "casaroot", self.casaroot_input.text(), config_file)
+            setConfigs("Paths", "pyraproot", self.pyraproot_input.text(), config_file)
+            setConfigs("Paths", "hdf5root", self.hdf5root_input.text(), config_file)
+            setConfigs("Paths", "wcsroot", self.wcsroot_input.text(), config_file)
+            setConfigs("Paths", "losotopath", self.losotoPath_input.text(), config_file)
+            setConfigs("Paths", "aoflagger", self.aoflagger_input.text(), config_file)
+            setConfigs("Paths", "wsclean_executable", self.wsclean_executable_input.text(), config_file)
+            setConfigs("Paths", "pythonpath", self.pythonpath_input.text(), config_file)
+            setConfigs("Paths", "task_file", self.task_file_input.text(), config_file)
 
-        print("Saving LAnDmARk configuration")
-        QMessageBox.about(self, "", "LAnDmARk configuration has been saved")
+            print("Saving LAnDmARk configuration")
+            QMessageBox.about(self, "", "LAnDmARk configuration has been saved")
 
 
 def main():
