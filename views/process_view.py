0a0fe45b2b4065d001e1c68f49ea786f64ac5d56
sklandrausis <janis@localhost.localdomain>
2020-04-30 17:24:51 +0000

    saving progress

--- views/process_view.py
+++ views/process_view.py
@@ -20,9 +20,23 @@ def get_pipeline_task(prefactor_path, parset_file):
 
 def run_pipeline(parset_file, config_file):
     try:
-        os.system('genericpipeline.py ' + parset_file + ' -c ' + config_file + ' -d')
+        os.system('nohup genericpipeline.py ' + parset_file + ' -c ' + config_file + ' -d -v')
     except:
-        print("Something went wrong")
+        print(sys.exc_info())
+
+
+def get_tasks_from_log_file(log_file):
+    tasks = []
+    if os.path.isfile(log_file):
+        with open(log_file, "rb") as parset_file:
+            lines = parset_file.readlines()
+            for line in lines:
+                line = line.decode("utf-8")
+                if "Beginning step" in line:
+                    task = line.split(":")[-1].split(" ")[-1].replace("\n", "")
+                    tasks.append(task.strip())
+
+    return tasks
 
 
 class ProcessView(QMainWindow):
@@ -39,7 +53,8 @@ class ProcessView(QMainWindow):
         self.imageDir = workingDir + "imaging_deep" + "/"
         self.timer = QtCore.QTimer()
         self.timer.setInterval(100)
-        self.step = 0
+        self.steps = []
+        self.threads = []
 
         prefactor_path = getConfigs("Paths", "prefactorpath", self.config_file)
         calibrator_parset_file = "Pre-Facet-Calibrator.parset"
@@ -58,6 +73,9 @@ class ProcessView(QMainWindow):
                 sys.exit(1)
         else:
             self.SASidsCalibrator = [int(id) for id in getConfigs("Data", "calibratorSASids", self.config_file).replace(" ", "").split(",")]
+
+        self.timer.timeout.connect(self.update_progress_bar)
+        threading.Thread(target=self.timer.start()).start()
 
         if getConfigs("Operations", "which_obj", "config.cfg") == "all" or len(getConfigs("Operations", "which_obj", "config.cfg")) == 0:
             calibrator_tasks = get_pipeline_task(prefactor_path, calibrator_parset_file)
@@ -96,16 +114,15 @@ class ProcessView(QMainWindow):
         else:
             self.tasks = get_pipeline_task(prefactor_path, calibrator_parset_file)
             self.create_init_view(self.SASidsCalibrator)
+            self.ids = self.SASidsCalibrator
 
             for id in self.SASidsCalibrator:
                 parsetCalib = self.calibratorDir + str(id) + "_RAW/" + "Pre-Facet-Calibrator.parset"
                 configCalib = self.calibratorDir + str(id) + "_RAW/" + "pipeline.cfg"
-                self.timer.start()
-                self.timer.timeout.connect(self.update_progress_bar)
-                self.log_file = getConfigs("Paths", "WorkingPath", "config.cfg") + "/" + \
-                                getConfigs("Data","TargetName", "config.cfg") + "/" + "calibrators/" + "pipeline_" + str(id) + ".log"
-                threading.Thread(target=run_pipeline, args=(parsetCalib, configCalib,)).start()  # run calibrator
-                self.progress_bars_index = self.SASidsCalibrator.index(id)
+                t = threading.Thread(target=run_pipeline, args=(parsetCalib, configCalib,)).start() # run calibrator
+                self.threads.append(t)
+                step = 0
+                self.steps.append(step)
 
     def create_init_view(self, SAS_ids):
         y_l = 10
@@ -131,39 +148,29 @@ class ProcessView(QMainWindow):
         self.setGeometry(10, 20, len("Running progress for SAS id ") + 560, 90 * len(SAS_ids))
 
     def update_progress_bar(self):
-        if self.step >= 100.0:
+        if sum(self.steps)/len(self.steps) >= 100.0:
             self.timer.stop()
-            return
 
-        executed_tasks = self.get_tasks_from_log_file()
-        if len(executed_tasks) == 0:
-            last_started_task = "not started"
-        else:
-            last_started_task = executed_tasks[-1]
+        for id in self.ids:
 
-        if last_started_task is not "not started":
-            try:
-                self.progress = len(executed_tasks)
-                self.step = self.get_progress_value()
-                self.progress_bars[self.progress_bars_index].setValue(self.step)
-                self.task_labels[self.progress_bars_index].setText("Prefactor started to execute task: " + last_started_task)
-            except ValueError as e:
-                print("ValueError", e, sys.exc_info()[0])
+            log_file = getConfigs("Paths", "WorkingPath", "config.cfg") + "/" + \
+                            getConfigs("Data", "TargetName", "config.cfg") + "/" + "calibrators/" + "pipeline_" + str(id) + ".log"
+            progress_bars_index = self.ids.index(id)
+
+            executed_tasks = get_tasks_from_log_file(log_file)
+            if len(executed_tasks) == 0:
+                last_started_task = "not started"
+            else:
+                last_started_task = executed_tasks[-1]
+
+            if last_started_task is not "not started":
+                try:
+                    self.progress = len(executed_tasks)
+                    self.steps[progress_bars_index] = self.get_progress_value()
+                    self.progress_bars[progress_bars_index].setValue(self.steps[progress_bars_index])
+                    self.task_labels[progress_bars_index].setText("Prefactor started to execute task: " + last_started_task)
+                except ValueError as e:
+                    print("ValueError", e, sys.exc_info()[0])
 
     def get_progress_value(self):
         return (self.progress / len(self.tasks))*100
-
-    def get_tasks_from_log_file(self):
-        tasks = []
-        log_file = self.log_file
-
-        if os.path.isfile(log_file):
-            with open(log_file, "rb") as parset_file:
-                lines = parset_file.readlines()
-                for line in lines:
-                    line = line.decode("utf-8")
-                    if "Beginning step" in line:
-                        task = line.split(":")[-1].split(" ")[-1].replace("\n", "")
-                        tasks.append(task.strip())
-
-        return tasks
