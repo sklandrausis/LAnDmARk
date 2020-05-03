86e6806449fbdfc8b228d4a249ed34192d8a4166
sklandrausis <janis.akmenskalns@gmail.com>
2020-05-03 16:38:18 +0300

    save progress

--- controllers/check_controller.py
+++ controllers/check_controller.py
@@ -5,21 +5,75 @@ from parsers._configparser import getConfigs
 
 
 class CheckController(QObject):
-    def __init__(self, ui, *args, **kwargs):
+    def __init__(self, check_view, *args, **kwargs):
         super(CheckController, self).__init__(*args, **kwargs)
-        self.ui = ui
+        self.check_view = check_view
+        self.ui = self.check_view._ui
+        self.config_file = "config.cfg"
         self.working_directory = getConfigs("Paths", "workingpath", "config.cfg") + "/" + getConfigs("Data", "targetname", "config.cfg")
 
-    def choose_combobox_changed(self):
-        if self.ui.choose_combobox.currentText() == "LAnDmARk inspection plots":
-            self.ui.inspection_plots_combobox.clear()
-            items = os.listdir(self.working_directory + "/LAnDmARk_aux/selection/")
-            items.extend(os.listdir(self.working_directory + "/LAnDmARk_aux/retrieve/"))
-            items.extend(os.listdir(self.working_directory + "/LAnDmARk_aux/stage/"))
-            self.ui.inspection_plots_combobox.addItems(items)
-        elif self.ui.inspection_plots_combobox.currentText == "Prefacotor calibrator inspection plots":
-            self.ui.clear()
-        elif self.ui.inspection_plots_combobox.currentText == "Prefactor target inspection plots":
-            self.ui.clear()
-        elif self.ui.inspection_plots_combobox.currentText == "Prefactor imaging inspection plots":
-            self.ui.clear()
+        self.project = getConfigs("Data", "PROJECTid", self.config_file)
+        self.targetSASids = getConfigs("Data", "targetSASids", self.config_file).replace(" ", "").split(",")
+        if len(getConfigs("Data", "calibratorSASids", self.config_file)) == 0:
+            if self.project == "MSSS_HBA_2013":
+                self.SASidsCalibrator = [int(id) - 1 for id in self.targetSASids]
+
+            else:
+                raise Exception("SAS id for calibrator is not set in config.cfg file")
+        else:
+            self.SASidsCalibrator = [int(id) for id in
+                                getConfigs("Data", "calibratorSASids", self.config_file).replace(" ", "").split(",")]
+
+        dirs = ["selection/", "retrieve/", "stage/"]
+        dirs = [self.working_directory + "/LAnDmARk_aux/" + d for d in dirs]
+
+        for id in self.SASidsCalibrator:
+            dirs.append(self.working_directory + "/calibrators/calibrators_results/results/inspection_" + str(id))
+
+        for id in self.targetSASids:
+            dirs.append(self.working_directory + "/targets/target_results/results/inspection_" + str(id))
+
+        dirs = ["selection/", "retrieve/", "stage/"]
+        dirs = [self.working_directory + "/LAnDmARk_aux/" + d for d in dirs]
+
+        for id in self.SASidsCalibrator:
+            dirs.append(self.working_directory + "/calibrators/calibrators_results/results/inspection_" + str(id))
+
+        for id in self.targetSASids:
+            dirs.append(self.working_directory + "/targets/target_results/results/inspection_" + str(id))
+
+        existing_dirs = [d for d in dirs if os.path.isdir(d)]
+        self.non_empty_dirs = [d for d in existing_dirs if len(os.listdir(d)) != 0]
+
+    def on_choose_combobox_changed(self):
+        items = []
+        if "LAnDmARk" in self.ui.choose_combobox.currentText():
+            for d in self.non_empty_dirs:
+                if "LAnDmARk" in d:
+                    items.extend(os.listdir(d))
+
+        for id in self.SASidsCalibrator:
+            if str(id) in self.ui.choose_combobox.currentText():
+                for d in self.non_empty_dirs:
+                    if str(id) in d:
+                        items = os.listdir(d)
+
+        for id in self.targetSASids:
+            if str(id) in self.ui.choose_combobox.currentText():
+                for d in self.non_empty_dirs:
+                    if str(id) in d:
+                        items = os.listdir(d)
+
+        self.ui.inspection_plots_combobox.clear()
+        self.ui.inspection_plots_combobox.addItems(items)
+
+    def on_inspection_plots_combobox_changed(self):
+        image = self.ui.inspection_plots_combobox.currentText()
+        path = self.working_directory
+
+        if "LAnDmARk" in self.ui.choose_combobox.currentText():
+            path += "/LAnDmARk_aux/"
+            sub_path = [d for d in os.listdir(path) if image in os.listdir(path + "/" + d)]
+            if len(sub_path) != 0:
+                path += sub_path[0] + "/"
+                self.check_view.set_image(path + image)
--- views/check_view.py
+++ views/check_view.py
@@ -1,5 +1,6 @@
 import os
 from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtGui import QPixmap
 from views.check_view_ui import Ui_check_view
 from controllers.check_controller import CheckController
 
@@ -12,13 +13,86 @@ class CheckView(QMainWindow):
 
         self._ui = Ui_check_view()
         self._ui.setupUi(self)
-        self.check_controller = CheckController(self._ui)
+        self.check_controller = CheckController(self)
         self.setWindowTitle('Check')
-        self.working_directory = getConfigs("Paths", "workingpath", "config.cfg") + "/" + getConfigs("Data", "targetname", "config.cfg")
+        self._ui.choose_combobox.currentTextChanged.connect(self.check_controller.on_choose_combobox_changed)
+        self._ui.inspection_plots_combobox.currentTextChanged.connect(self.check_controller.on_inspection_plots_combobox_changed)
 
-        self._ui.choose_combobox.currentTextChanged.connect(self.check_controller.choose_combobox_changed)
-        items = os.listdir(self.working_directory + "/LAnDmARk_aux/selection/")
-        items.extend(os.listdir(self.working_directory + "/LAnDmARk_aux/retrieve/"))
-        items.extend(os.listdir(self.working_directory + "/LAnDmARk_aux/stage/"))
-        self._ui.inspection_plots_combobox.addItems(items)
+        self.config_file = "config.cfg"
+        self.working_directory = getConfigs("Paths", "workingpath", "config.cfg") + "/" + getConfigs("Data", "targetname", "config.cfg")
+        project = getConfigs("Data", "PROJECTid", self.config_file)
+
+        targetSASids = getConfigs("Data", "targetSASids", self.config_file).replace(" ", "").split(",")
+        if len(getConfigs("Data", "calibratorSASids", self.config_file)) == 0:
+            if project == "MSSS_HBA_2013":
+                SASidsCalibrator = [int(id) - 1 for id in targetSASids]
+
+            else:
+                raise Exception("SAS id for calibrator is not set in config.cfg file")
+        else:
+            SASidsCalibrator = [int(id) for id in getConfigs("Data", "calibratorSASids", self.config_file).replace(" ", "").split(",")]
+
+        dirs = ["selection/", "retrieve/",  "stage/"]
+        dirs = [self.working_directory + "/LAnDmARk_aux/" + d for d in dirs]
+
+        for id in SASidsCalibrator:
+            dirs.append(self.working_directory + "/calibrators/calibrators_results/results/inspection_" + str(id))
+
+        for id in targetSASids:
+            dirs.append(self.working_directory + "/targets/target_results/results/inspection_" + str(id))
+
+        existing_dirs = [d for d in dirs if os.path.isdir(d)]
+        non_empty_dirs = [d for d in existing_dirs if len(os.listdir(d)) != 0]
+        dir_length = [len(os.listdir(d)) for d in existing_dirs]
+
+        self._ui.choose_combobox.clear()
+        for d in non_empty_dirs:
+            if "LAnDmARk" in d:
+                self._ui.choose_combobox.addItem("LAnDmARk inspection plots")
+
+            for id in SASidsCalibrator:
+                if str(id) in d:
+                    self._ui.choose_combobox.addItem("Prefacotor calibrator inspection plots for SAS id " + str(id))
+
+            for id in targetSASids:
+                if str(id) in d:
+                    self._ui.choose_combobox.addItem("Prefactor target inspection plots for SAS id " + str(id))
+
+        if sum(dir_length) == 0:
+            self._ui.inspection_plot.setText("No inspection plot available")
+        else:
+            items = []
+            image = self._ui.inspection_plots_combobox.currentText()
+            if "LAnDmARk" in self._ui.choose_combobox.currentText():
+                for d in non_empty_dirs:
+                    if "LAnDmARk" in d:
+                        items.extend(os.listdir(d))
+
+                path = self.working_directory + "/LAnDmARk_aux/" + [d for d in os.listdir(self.working_directory + "/LAnDmARk_aux/") if image in os.listdir(self.working_directory + "/LAnDmARk_aux/" + d)][0] + "/"
+                self.set_image(path + image)
+
+            for id in SASidsCalibrator:
+                if str(id) in self._ui.choose_combobox.currentText():
+                    for d in non_empty_dirs:
+                        if str(id) in d:
+                            items = os.listdir(d)
+                            path = d + "/"
+                            self.set_image(path + image)
+
+            for id in targetSASids:
+                if str(id) in self._ui.choose_combobox.currentText():
+                    for d in non_empty_dirs:
+                        if str(id) in d:
+                            items = os.listdir(d)
+                            path = d
+                            self.set_image(path + image)
+
+            self._ui.inspection_plots_combobox.clear()
+            self._ui.inspection_plots_combobox.addItems(items)
+
+    def set_image(self, image):
+        self.pixmap = QPixmap(image)
+        self._ui.inspection_plot.setPixmap(self.pixmap)
+        self.resize(self.pixmap.width(), self.pixmap.height())
+
 
