47d117f6c240fdab4d52d49b1ae043cbf0ef7476
sklandrausis <janis.akmenskalns@gmail.com>
2020-08-19 14:07:32 +0300

    optimized code

--- operations/process.py
+++ operations/process.py
@@ -45,17 +45,20 @@ class Process(Operation):
                 parset_calib = calibrator_dir + str(id) + "_RAW/" + "Pre-Facet-Calibrator.parset"
                 config_calib = calibrator_dir + str(id) + "_RAW/" + "pipeline.cfg"
                 tc = threading.Thread(target=run_pipeline, args=(parset_calib, config_calib,))
+                tc.setName("calibrator" + "_" + str(id))
                 threads.append(tc)
 
             for id in sas_ids_target:
                 parset_target = target_dir + str(id) + "_RAW/" + "Pre-Facet-Target.parset"
                 config_target = target_dir + str(id) + "_RAW/" + "pipeline.cfg"
                 tt = threading.Thread(target=run_pipeline, args=(parset_target, config_target,))
+                tt.setName("target" + "_" + str(id))
                 threads.append(tt)
 
             parset_image = image_dir + "Pre-Facet-Image.parset"
             config_image = image_dir + "pipeline.cfg"
             ti = threading.Thread(target=run_pipeline, args=(parset_image, config_image,))
+            ti.setName("imaging")
             threads.append(ti)
 
         elif getConfigs("Operations", "which_obj", "config.cfg") == "targets":
@@ -64,17 +67,51 @@ class Process(Operation):
                 parset_target = target_dir + str(id) + "_RAW/" + "Pre-Facet-Target.parset"
                 config_target = target_dir + str(id) + "_RAW/" + "pipeline.cfg"
                 tt = threading.Thread(target=run_pipeline, args=(parset_target, config_target,))
+                tt.setName("target" + "_" + str(id))
                 threads.append(tt)
         else:
             for id in sas_ids_calibrator:
                 parset_calib = calibrator_dir + str(id) + "_RAW/" + "Pre-Facet-Calibrator.parset"
                 config_calib = calibrator_dir + str(id) + "_RAW/" + "pipeline.cfg"
                 tc = threading.Thread(target=run_pipeline, args=(parset_calib, config_calib,))
+                tc.setName("calibrator" + "_" + str(id))
                 threads.append(tc)
 
         for thread in threads:
-            thread.start()
-            thread.join()
+            if "_" in thread.getName():
+                type_of_process = thread.getName().split("_")[0]
+                sas_id = thread.getName().split("_")[1]
+            else:
+                type_of_process = "imaging"
+
+            if type_of_process == "calibrator":
+                calibrator_raw_files = [file for file in os.listdir(calibrator_dir + sas_id + "_RAW/") if ".MS" in file]
+                if len(calibrator_raw_files) == 0:
+                    print("For sas ir " + sas_id + " there is no calibrator raw data")
+                else:
+                    thread.start()
+                    thread.join()
+
+            elif type_of_process == "target":
+                target_raw_files = [file for file in os.listdir(target_dir + sas_id + "_RAW/") if ".MS" in file]
+                if len(target_raw_files) == 0:
+                    print("For sas ir " + sas_id + " there is no target raw data")
+                elif not os.path.isfile(calibrator_dir + "calibrators_results/results/cal_values_" +
+                                        str(sas_ids_calibrator[sas_ids_target.index(int(sas_id))]) + "/cal_solutions.h5"):
+                    print("For sas id " + sas_id + " calibration solution file do not exist")
+                else:
+                    thread.start()
+                    thread.join()
+
+            if type_of_process == "imaging":
+                target_result_files = [file for file in os.listdir(target_dir + "targets_results/results")
+                                       if "pre-cal.ms" in file]
+                if len(target_result_files) == 0:
+                    print("target results do not exist")
+                else:
+                    print("processing imaging")
+                    thread.start()
+                    thread.join()
 
     @property
     def name(self):
